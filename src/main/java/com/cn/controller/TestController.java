d50ece1dc1e1cf42937c911103e32e5695f4b8ef
feihu.ying <yienhui123>
2020-06-24 13:35:47 +0800

    controller提交2

--- src/main/java/com/cn/controller/TestController.java
+++ src/main/java/com/cn/controller/TestController.java
@@ -14,7 +14,7 @@ public class TestController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        //我现在也要修改这里代码
+        //我现在也要修改这里信息
         return "hello";
     }
 }
