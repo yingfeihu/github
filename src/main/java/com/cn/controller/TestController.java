a78c4e0ab1ed1f1fc60bb71c64c5c89ced6ec4bc
feihu.ying <yienhui123>
2020-06-24 11:39:44 +0800

    controller提交

---
+++ src/main/java/com/cn/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.cn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Author: 应飞虎
+ * @Date: 2020/6/24 11:27
+ * @Desc:
+ */
+@Controller
+@ResponseBody
+public class TestController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "hello";
+    }
+}
